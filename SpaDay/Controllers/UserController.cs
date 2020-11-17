c2aac37ae6f8277daad7211957dd5fa430c75ec2
Queue <elbertq2@gmail.com>
2020-11-16 20:48:58 -0600

    finishing up studio

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index(string Username, string Email, string Password1, string Verify)
+        {
+
+            ViewBag.name = Username;
+            return SubmitAddUserForm(new User(Username, Email, Password1), Verify);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitAddUserForm(User newUser, string Verify)
+        {
+            // add form submission handling code here
+            if (Verify == newUser.Password)
+            {
+                ViewBag.pword = Verify;
+                return View("Index");
+
+            } else
+            {
+                return Redirect("/User/Add");
+            } 
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public String Username { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public User()
+        {
+        }
+        public User(string username, string email, string password)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+    }
+
+
+}
