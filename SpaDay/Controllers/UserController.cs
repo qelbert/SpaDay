cd8bd0dfefebd1d22c21bb97695778f2f72aa141
Queue <elbertq2@gmail.com>
2020-12-05 12:50:30 -0600

    Completed ModelView/Validation Studio with Bonus

--- SpaDay/Controllers/UserController.cs
+++ SpaDay/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SpaDay.Models;
+using SpaDay.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,26 +20,86 @@ namespace SpaDay.Controllers
 
         public IActionResult Add()
         {
-            return View();
+            AddUserViewModel addUserViewModel = new AddUserViewModel();
+            return View(addUserViewModel);
         }
+
+
+        //[HttpPost]
+        //[Route("/user")]
+        //public IActionResult SubmitAddUserForm(AddUserViewModel addUserViewModel)
+        //{
+        //    if (ModelState.IsValid) //this line or entire if statement seems to be the issue
+        //    {
+        //        User newUser = new User
+        //        {
+        //            Username = addUserViewModel.Username,
+        //            Password = addUserViewModel.Password,
+        //            Email = addUserViewModel.Email
+        //        };
+
+        //        ViewBag.user = newUser;
+
+        //        return View("Index");
+        //    }
+
+        //    return View(addUserViewModel);
+        //}
+
+
+        //[HttpPost]
+        //[Route("/user")]
+        //////public IActionResult SubmitAddUserForm(User newUser, string verify)
+        //public IActionResult SubmitAddUserForm(AddUserViewModel addUserViewModel)
+        //{
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        if (addUserViewModel.Password == addUserViewModel.VerifyPassword)
+        //        {
+
+        //            User newUser = new User
+        //            {
+        //                Username = addUserViewModel.Username,
+        //                Password = addUserViewModel.Password,
+        //                Email = addUserViewModel.Email
+        //            };
+
+        //            return View("Index", newUser);
+        //        }
+
+        //        else
+        //        {
+        //            ViewBag.error = "Passwords do not match! Try again!";
+        //            return View("Add", addUserViewModel);
+        //        }
+        //    }
+
+        //    return View("Add", addUserViewModel);
+        //}
 
         [HttpPost]
         [Route("/user")]
-        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        public IActionResult SubmitAddUserForm(AddUserViewModel addUserViewModel)
         {
-            if (newUser.Password == verify)
+
+            if (ModelState.IsValid)
             {
-                ViewBag.user = newUser;
-                return View("Index");
+                User newUser = new User
+                {
+                    Username = addUserViewModel.Username,
+                    Password = addUserViewModel.Password,
+                    Email = addUserViewModel.Email
+                };
+                
+                return View("Index", addUserViewModel);
             }
+
             else
+
             {
-                ViewBag.error = "Passwords do not match! Try again!";
-                ViewBag.userName = newUser.Username;
-                ViewBag.eMail = newUser.Email;
-                return View("Add");
+               return View("Add", addUserViewModel);
             }
         }
-
     }
 }
---
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Please enter a name that is 5-15 characters in length." )]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Please enter a password that is 6-20 characters in length.")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Verifying your password is required")]
+        [Compare("Password", ErrorMessage = "Oops, these passwords do not match!")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Please enter a password that is 6-20 characters in length.")]
+        public string VerifyPassword { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
